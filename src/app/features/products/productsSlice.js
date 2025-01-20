de54de1fb405956f1895301fdc5e6aee690d86d2
ahmed-al-farouq <mediocre23534@gmail.com>
2025-01-20 11:08:59 +0200

    fetching 3 products' categories instead of all

--- src/app/features/products/productsSlice.js
+++ src/app/features/products/productsSlice.js
@@ -5,30 +5,9 @@ export const fetchProduts = createAsyncThunk(
   "products/fetchProduts",
   async (_, thunkAPI) =>
     fetchProductsByCategories(thunkAPI, [
-      "beauty",
-      "fragrances",
-      "furniture",
-      "groceries",
-      "home-decoration",
-      "kitchen-accessories",
-      "laptops",
       "mens-shirts",
       "mens-shoes",
       "mens-watches",
-      "mobile-accessories",
-      "motorcycle",
-      "skin-care",
-      "smartphones",
-      "sports-accessories",
-      "sunglasses",
-      "tablets",
-      "tops",
-      "vehicle",
-      "womens-bags",
-      "womens-dresses",
-      "womens-jewellery",
-      "womens-shoes",
-      "womens-watches",
     ])
 );
 
