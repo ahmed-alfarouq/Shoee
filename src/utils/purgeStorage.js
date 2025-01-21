93ac99b78c3609b9e2eee113e685380d4cfebff7
ahmed-al-farouq <mediocre23534@gmail.com>
2025-01-21 18:45:37 +0200

    remove console

--- src/utils/purgeStorage.js
+++ src/utils/purgeStorage.js
@@ -1,9 +1,7 @@
 const purgeStorage = (lastUpdated, persistor) => {
   const EXPIRE_TIME = 12 * 60 * 60 * 1000;
   const NOW = Date.now();
-  console.log("out ", persistor.getState());
   if (lastUpdated && NOW - lastUpdated > EXPIRE_TIME) {
-    console.log("in ", lastUpdated, persistor, NOW);
     persistor.purge();
   }
 };
