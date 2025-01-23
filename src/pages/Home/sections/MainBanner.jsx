024c3a44cabec70005b86a7cb7d41ff6d0f39522
ahmed-al-farouq <mediocre23534@gmail.com>
2025-01-23 07:38:07 +0200

    made main banner more responsive

--- src/pages/Home/sections/MainBanner.jsx
+++ src/pages/Home/sections/MainBanner.jsx
@@ -7,22 +7,24 @@ import { FaAnglesRight } from "react-icons/fa6";
 
 const MainBanner = () => {
   return (
-    <section className="banner container">
-      <div className="left-wrap">
-        <h1 className="title">All you need is here!</h1>
-        <p className="content">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae
-          consequuntur, mollitia culpa repudiandae voluptatem ullam dicta nisi
-          laborum voluptates veniam vero non tempore dignissimos.
-        </p>
+    <section className="banner">
+      <div className="container">
+        <div className="left-wrap">
+          <h1 className="title">All you need is here!</h1>
+          <p className="content">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae
+            consequuntur, mollitia culpa repudiandae voluptatem ullam dicta nisi
+            laborum voluptates veniam vero non tempore dignissimos.
+          </p>
 
-        <Link to="/shop" className="btn">
-          <FaAnglesRight />
-          explore store
-        </Link>
-      </div>
-      <div className="right-wrap">
-        <img src={bannerImage} alt="Banner" loading="lazy" />
+          <Link to="/shop" className="btn">
+            <FaAnglesRight />
+            explore store
+          </Link>
+        </div>
+        <div className="right-wrap">
+          <img src={bannerImage} alt="Banner" loading="lazy" />
+        </div>
       </div>
     </section>
   );
