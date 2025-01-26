752ba5dd06e10d53e8a090fa43cb66c0906938f4
ahmed-al-farouq <mediocre23534@gmail.com>
2025-01-26 09:55:23 +0200

    Add vercel json, and change log

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,4 +8,6 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
