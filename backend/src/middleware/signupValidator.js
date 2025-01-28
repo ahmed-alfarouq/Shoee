5cc22974e36acfbcec6c7b7c7066b172e5455944
ahmed-al-farouq <mediocre23534@gmail.com>
2025-01-28 19:35:57 +0200

    add verify email, reset password pages, and thier functionalities

--- backend/src/middleware/signupValidator.js
+++ backend/src/middleware/signupValidator.js
@@ -1,15 +1,26 @@
 import { body, validationResult } from "express-validator";
 
 const signupValidator = [
-  body("email").isEmail().withMessage("Please provide a valid email address"),
+  body("email")
+    .trim()
+    .normalizeEmail({
+      all_lowercase: true,
+      gmail_remove_dots: false,
+    })
+    .isEmail()
+    .withMessage("Please provide a valid email address!"),
 
   body("password")
+    .trim()
     .isLength({ min: 8 })
-    .withMessage("Password must be at least 8 characters long"),
+    .withMessage("Password must be at least 8 characters long!")
+    .escape(),
 
   body("username")
+    .trim()
     .isAlphanumeric()
-    .withMessage("Username must be alphanumeric"),
+    .withMessage("Username must be Alphanumeric!")
+    .escape(),
 
   (req, res, next) => {
     const errors = validationResult(req);
---
+++ backend/src/utils/sendEmail.js
@@ -0,0 +1,35 @@
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
+
+// Function to send an email
+const sendEmail = async (to, subject, text) => {
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to,
+    subject,
+    text,
+  };
+
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email sent: " + info.response);
+    return true;
+  } catch (error) {
+    console.error("Error sending email:", error);
+    return Promise.reject("Error sending email:", error);
+  }
+};
+
+export default sendEmail;
