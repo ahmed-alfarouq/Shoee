6ba770c77c0a0b4f2074b63472a5d71bddf15ac9
ahmed-al-farouq <mediocre23534@gmail.com>
2025-02-02 18:59:14 +0200

    remove unneccessary code

--- backend/src/middleware/validations.js
+++ backend/src/middleware/validations.js
@@ -81,7 +81,6 @@ const billingDetailsValidation = [
 
   (req, res, next) => {
     const errors = validationResult(req);
-    console.log(errors);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
