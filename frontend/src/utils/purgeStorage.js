9a0a72fcfdaee24874dfb099eb166955f306e644
ahmed-alfarouq <mediocre23534@gmail.com>
2025-02-16 15:27:00 +0200

    fix purging data, and fetching products issue

--- frontend/src/utils/purgeStorage.js
+++ frontend/src/utils/purgeStorage.js
@@ -1,10 +1,12 @@
 import { setLoading } from "../app/features/main/mainSlice";
 
-const purgeStorage = (lastUpdated, persistor, dispatch) => {
+const purgeStorage = async (lastUpdated, persistor, dispatch) => {
   const EXPIRE_TIME = 12 * 60 * 60 * 1000;
   const NOW = Date.now();
+
   if (lastUpdated && NOW - lastUpdated > EXPIRE_TIME) {
-    persistor.purge();
+    dispatch(setLoading(true));
+    await persistor.purge();
     dispatch(setLoading(false));
   }
 };
