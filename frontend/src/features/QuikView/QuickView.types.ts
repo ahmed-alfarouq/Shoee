e8c1f6fc02b0d8b7becf5e0de0c0114dcae323b7
ahmed-al-farouq <mediocre23534@gmail.com>
2025-11-17 07:36:07 +0200

    feat: enhance QuickViewProps interface with additional properties for sale indication and modal visibility

--- frontend/src/features/QuikView/QuickView.types.ts
+++ frontend/src/features/QuikView/QuickView.types.ts
@@ -1,7 +1,23 @@
 import type { ProductProps } from "@/types/index.types";
 
 export interface QuickViewProps {
+  /**
+   * REQUIRED: The product object
+   */
   item: ProductProps;
+
+  /**
+   * OPTIONAL: If true, Sale! badge will be displayed
+   */
+  onSale?: boolean;
+
+  /**
+   * REQUIRED: Decides if modal is displayed or not
+   */
   hidden: boolean;
+
+  /**
+   * REQUIRED: Callback fired when clicking the BG overlay or the close icon.
+   */
   close: () => void;
 }
