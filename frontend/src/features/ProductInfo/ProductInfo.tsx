e6780b6194af94aeaf548c5bdd3bd107280d27df
ahmed-al-farouq <mediocre23534@gmail.com>
2025-11-17 07:35:28 +0200

    style: add ProductInfo.module.scss for styling and update ProductInfo component

--- frontend/src/features/ProductInfo/ProductInfo.tsx
+++ frontend/src/features/ProductInfo/ProductInfo.tsx
@@ -1,19 +1,21 @@
+import styles from "./ProductInfo.module.scss";
+
 import { IoMdArrowDroprightCircle } from "react-icons/io";
 
 import type { ProductInfoProps } from "./ProductInfo.types";
 
 const ProductInfo = ({ item }: ProductInfoProps) => {
   return (
-    <ul className="info">
-      <li>Free shipping on orders over $100!</li>
-      <li>
+    <ul className={styles.info}>
+      <li className={styles.info_item}>Free shipping on orders over $100!</li>
+      <li className={styles.info_item}>
         <IoMdArrowDroprightCircle aria-hidden="true" /> {item.returnPolicy}
       </li>
-      <li>
+      <li className={styles.info_item}>
         <IoMdArrowDroprightCircle aria-hidden="true" />{" "}
         {item.shippingInformation}
       </li>
-      <li>
+      <li className={styles.info_item}>
         <IoMdArrowDroprightCircle aria-hidden="true" />{" "}
         {item.warrantyInformation}
       </li>
