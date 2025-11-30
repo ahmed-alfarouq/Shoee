4f4435ccd734f47b15bd24c685a4ead530788e75
ahmed-al-farouq <mediocre23534@gmail.com>
2025-11-30 16:39:18 +0200

    refactor: update error messages in useCart hooks to include <CartProvider> for clarity

--- frontend/src/hooks/useCart.ts
+++ frontend/src/hooks/useCart.ts
@@ -8,13 +8,14 @@ import {
 
 export const useCartState = () => {
   const ctx = useContext(CartStateContext);
-  if (!ctx) throw new Error("useCartState must be used inside CartProvider");
+  if (!ctx) throw new Error("useCartState must be used inside <CartProvider>");
   return ctx;
 };
 
 export const useCartDispatch = () => {
   const ctx = useContext(CartDispatchContext);
-  if (!ctx) throw new Error("useCartDispatch must be used inside CartProvider");
+  if (!ctx)
+    throw new Error("useCartDispatch must be used inside <CartProvider>");
   return ctx;
 };
 
--- frontend/src/pages/Cart/sections/CartTotals.tsx
+++ frontend/src/pages/Cart/sections/CartTotals.tsx
@@ -1,20 +1,13 @@
 import { Link } from "react-router-dom";
-import { Activity, useState } from "react";
 
 import styles from "../Cart.module.scss";
 
 import { Button } from "@/components/Button";
-import { CouponForm } from "@/features/CouponForm";
 
 import { useCartState } from "@/hooks/useCart";
 
 const CartTotals = () => {
   const { total } = useCartState();
-  const [showCouponForm, setShowCouponForm] = useState(false);
-
-  const toggleCouponForm = () => setShowCouponForm(!showCouponForm);
-
-  const handleCouponSubmit = () => console.log("Coupon submited!");
 
   return (
     <section className={styles.cart_total}>
@@ -32,37 +25,10 @@ const CartTotals = () => {
               </span>
             </td>
           </tr>
-          <tr className={styles.order_total}>
-            <th scope="row">Total</th>
-            <td>
-              <strong>
-                <span className={styles.amount}>
-                  <bdi>
-                    <span className={styles.currency_symbol}>$</span>
-                    {total.toFixed(2)}
-                  </bdi>
-                </span>
-              </strong>
-            </td>
-          </tr>
         </tbody>
       </table>
 
       <div className={styles.footer}>
-        {!showCouponForm && (
-          <Button
-            size="sm"
-            variant="ghost"
-            aria-expanded={showCouponForm ? "true" : "false"}
-            onClick={toggleCouponForm}
-            aria-controls="coupon-form"
-          >
-            Have a coupon?
-          </Button>
-        )}
-        <Activity mode={showCouponForm ? "visible" : "hidden"}>
-          <CouponForm onSuccess={handleCouponSubmit} />
-        </Activity>
         <Button asChild>
           <Link to="/checkout">Proceed to checkout</Link>
         </Button>
