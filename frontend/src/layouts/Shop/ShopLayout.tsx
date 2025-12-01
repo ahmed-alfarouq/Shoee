c98e52a3f036f637cee62276ec63daa23474c531
ahmed-al-farouq <mediocre23534@gmail.com>
2025-12-01 14:27:24 +0200

    refactor: update DashboardLayout component and integrate ScrollToTop in ShopLayout for improved navigation

--- frontend/src/layouts/Shop/ShopLayout.tsx
+++ frontend/src/layouts/Shop/ShopLayout.tsx
@@ -4,10 +4,12 @@ import CartProdiver from "@/context/cart";
 
 import { Navbar } from "./Navbar";
 import { Footer } from "./Footer";
+import { ScrollToTop } from "@/shared/ScrollToTop";
 
 const ShopLayout = () => {
   return (
     <CartProdiver>
+      <ScrollToTop />
       <Navbar />
       <main>
         <Outlet />
