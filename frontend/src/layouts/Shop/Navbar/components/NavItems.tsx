149f4654db2eb813b3611c974a4c2d923fd41216
ahmed-al-farouq <mediocre23534@gmail.com>
2025-12-04 16:34:34 +0200

    feat: enhance navigation links with active state styling and adjust border thickness

--- frontend/src/layouts/Shop/Navbar/components/NavItems.tsx
+++ frontend/src/layouts/Shop/Navbar/components/NavItems.tsx
@@ -1,27 +1,49 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+
 import styles from "../Navbar.module.scss";
 
 const NavItems = () => {
+  const pathname = useLocation().pathname;
   return (
     <ul className={styles.menu}>
       <li className={styles.nav_item}>
-        <Link to="/" className={styles.nav_link}>
+        <Link
+          to="/"
+          className={`${styles.nav_link} ${
+            pathname === "/" ? styles.active : ""
+          }`}
+        >
           Home
         </Link>
       </li>
       <li className={styles.nav_item}>
-        <Link to="/products" className={styles.nav_link}>
+        <Link
+          to="/products"
+          className={`${styles.nav_link} ${
+            pathname === "/products" ? styles.active : ""
+          }`}
+        >
           All products
         </Link>
       </li>
       <li className={styles.nav_item}>
-        <Link to="/contactus" className={styles.nav_link}>
+        <Link
+          to="/contactus"
+          className={`${styles.nav_link} ${
+            pathname === "/contactus" ? styles.active : ""
+          }`}
+        >
           Contact
         </Link>
       </li>
 
       <li className={styles.nav_item}>
-        <Link to="/account" className={styles.nav_link}>
+        <Link
+          to="/account"
+          className={`${styles.nav_link} ${
+            pathname === "/account" ? styles.active : ""
+          }`}
+        >
           My account
         </Link>
       </li>
