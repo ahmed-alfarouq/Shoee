374305d1b3dd4f2ea4eb6d243d50e2dc31cfa289
ahmed-al-farouq <mediocre23534@gmail.com>
2025-12-05 23:04:08 +0200

    refactor: update ResetPassword component to use AuthWrapper and simplify layout

--- frontend/src/pages/ResetPassword/ResetPassword.tsx
+++ frontend/src/pages/ResetPassword/ResetPassword.tsx
@@ -1,28 +1,13 @@
-import { Link, useSearchParams } from "react-router-dom";
+import { AuthWrapper } from "@/features/Auth/AuthWrapper";
+import { ResetPasswordForm } from "@/features/Auth/ResetPasswordForm";
 
-import ResetPasswordForm from "@/features/Auth/ResetPasswordForm/ResetPasswordForm";
+const bottomLinks = [{ url: "/sign-in", text: "Back to Sign In" }];
 
 const ResetPassword = () => {
-  const [searchParams] = useSearchParams();
-  const message = searchParams.get("message");
-
   return (
-    <section className="auth">
-      <div className="container">
-        <h1 className="title">Reset password</h1>
-        {message && (
-          <p className="message">
-            ({message}:{" "}
-            <Link to="/login" replace>
-              Go to login
-            </Link>
-            )
-          </p>
-        )}
-
-        <ResetPasswordForm />
-      </div>
-    </section>
+    <AuthWrapper title="Reset password" bottomLinks={bottomLinks}>
+      <ResetPasswordForm />
+    </AuthWrapper>
   );
 };
 
