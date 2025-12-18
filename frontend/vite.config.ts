564e31787d33bbaa21f65e735c00bd518df98c6c
ahmed-al-farouq <mediocre23534@gmail.com>
2025-12-19 00:31:35 +0200

    feat: add rollup-plugin-visualizer package to view site stats for performance

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,11 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), visualizer({ open: true })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
