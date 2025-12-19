086baf2657c2b0233a7300c145700b49ff9e2e09
ahmed-al-farouq <mediocre23534@gmail.com>
2025-12-19 21:12:48 +0200

    fix: redirect user from checkout page if cart is empty

--- frontend/src/pages/Checkout/Checkout.tsx
+++ frontend/src/pages/Checkout/Checkout.tsx
@@ -1,9 +1,22 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 import styles from "./Checkout.module.scss";
 
 import Sidebar from "./sections/Sidebar";
 import Addresses from "./sections/Addresses";
 
+import { useCartState } from "@/hooks/useCart";
+
 const Checkout = () => {
+  const { items } = useCartState();
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (items.length <= 0) navigate("/products", { replace: true });
+  }, [items.length, navigate]);
+
   return (
     <section className={`${styles.checkout} container`}>
       <h1 className={styles.title}>Checkout</h1>
